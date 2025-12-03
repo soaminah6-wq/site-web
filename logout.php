484bd4058e609c55bf11de6785418735bbbe8476
ifztrk <i.ozturk@ecole-ipssi.net>
2025-12-03 11:07:38 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-session_unset();    // Supprime toutes les variables de session
+session_unset();    // Supprime  les variables de session
 session_destroy();  // Détruit la session
 header("Location: login.php"); // Redirige vers la page de connexion
 exit;
 ?>
+
